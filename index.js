c5f7c1a6a61dfd3afc543695557d215d89cfeee3
Rodrigo Delgado <rod.delgado85@gmail.com>
2016-12-11 17:33:36 +1300

    pushing everything up

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('app is running on PORT', app.get('port'));
+});
